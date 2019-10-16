53c30ff871a598062046a01bdc8d3681a1971e3b
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-10-16 10:14:00 +0200

    Fin module accueil

--- src/app/accueil/accueil.page.ts
+++ src/app/accueil/accueil.page.ts
@@ -1,39 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UtilisateurProfil} from '../entities/UtilisateurProfil';
-import { Plugins } from '@capacitor/core';
-import {CommuneRecherche} from "../entities/CommuneRecherche";
-import {error} from "util";
-const { Geolocation } = Plugins;
+import {Plugins} from '@capacitor/core';
+import {PositionService} from './position.service';
+import {CommuneCarte} from '../entities/CommuneCarte';
+import {flatMap} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/internal-compatibility';
+import {Router} from "@angular/router";
+
+
+const {Geolocation} = Plugins;
 
 @Component({
-  selector: 'app-accueil',
-  templateUrl: './accueil.page.html',
-  styleUrls: ['./accueil.page.scss'],
+    selector: 'app-accueil',
+    templateUrl: './accueil.page.html',
+    styleUrls: ['./accueil.page.scss'],
 })
 export class AccueilPage implements OnInit {
 
-  /**
-   * Utilisateur connecté
-   */
-  utilisateur: UtilisateurProfil;
-  communeRecherche: CommuneRecherche;
-  loading: boolean;
+    /**
+     * Utilisateur connecté
+     */
+    utilisateur: UtilisateurProfil;
 
-  constructor() { }
+    /**
+     * Cet attribut représente l'affichage ou non de l'icone de chargement
+     */
+    loading: boolean;
 
-  async rechercheParPosition() {
-    this.loading = true;
-    const coordinates = await Geolocation.getCurrentPosition().then((posInfos) => {
-      console.log(posInfos);
-      setTimeout(() => this.loading = false, 5000);
+    communeRecherche: CommuneCarte;
 
-    }, (err) => {
-      console.log(err());
-      setTimeout(() => this.loading = false, 5000);
-    });
-  }
+    constructor(private positionService: PositionService, private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    /**
+     * Récupère la géolocalisation de l'utilisateur, s'il l'accepte, et lui renvoie la commune enregistrée la plus proche
+     * de sa position
+     */
+    async rechercheParPosition() {
+        this.loading = true;
+
+        fromPromise(Geolocation.getCurrentPosition())
+            .pipe(
+                flatMap(posInfos => this.positionService.recupererCommuneLaPlusProche(posInfos))
+            )
+            .subscribe(commune => {
+                    this.communeRecherche = commune;
+                    this.loading = false;
+                    console.log(this.communeRecherche);
+                },
+                () => this.loading = false);
+    }
+
+    ngOnInit() {
+    }
 
 }
