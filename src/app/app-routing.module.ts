90879d0a54cd25afafd15093d1036b01b331c285
jbmerand <jbmerand@icloud.com>
2019-10-16 12:32:09 +0200

    cclean-air-mobile : formulaire d'inscription, infos RGPD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,28 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'accueil',
-    pathMatch: 'full'
-  },
-  {
-    path: 'recherche',
-    loadChildren: () => import('./recherche/recherche.module').then(m => m.RecherchePageModule)
-  },
-  {
-    path: 'inscription',
-    loadChildren: () => import('./inscription/inscription.module').then(m => m.InscriptionPageModule)
-  },
-  {path: 'accueil', loadChildren: './accueil/accueil.module#AccueilPageModule'},
-  {path: 'mon-profil', loadChildren: './mon-profil/mon-profil.module#MonProfilPageModule'},
-  {path: 'authentification', loadChildren: './authentification/authentification.module#AuthentificationPageModule'}
-
-
+    {
+        path: '',
+        redirectTo: 'accueil',
+        pathMatch: 'full'
+    },
+    {
+        path: 'inscription',
+        loadChildren: () => import('./inscription/inscription.module').then(m => m.InscriptionPageModule)
+    },
+    {
+        path: 'accueil',
+        loadChildren: () => import('./accueil/accueil.module').then(m => m.AccueilPageModule)
+    },
+    {
+        path: 'mon-profil',
+        loadChildren: () => import('./mon-profil/mon-profil.module').then(m => m.MonProfilPageModule)
+    },
+    {
+        path: 'authentification',
+        loadChildren: () => import('./authentification/authentification.module').then(m => m.AuthentificationPageModule)
+    }
 ];
 
 @NgModule({
