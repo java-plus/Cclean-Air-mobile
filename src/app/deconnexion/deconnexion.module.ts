19f4747907d16719bc4d19e131611f72e2a5eca8
jbmerand <jbmerand@icloud.com>
2019-10-17 17:36:07 +0200

    cclean-air-mobile : déconnexion

---
+++ src/app/deconnexion/deconnexion.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {IonicModule} from '@ionic/angular';
+import {DeconnexionPage} from './deconnexion.page';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DeconnexionPage
+    }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [DeconnexionPage]
+})
+export class DeconnexionPageModule {
+}
