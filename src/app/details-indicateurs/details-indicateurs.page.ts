57fdb9d9e88475f2fe689441fcbcca847ddde0cf
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-10-25 10:35:13 +0200

    Suppression d'indicateurs

--- src/app/details-indicateurs/details-indicateurs.page.ts
+++ src/app/details-indicateurs/details-indicateurs.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { CommuneService } from '../services/commune.service';
 import { CommuneDtoVisualisation } from '../entities/CommuneDtovisualisation';
 import { ConditionMeteoDtoVisualisation } from '../entities/ConditionMeteoDtoVisualisation';
@@ -7,6 +7,8 @@ import { PolluantDtoVisualisation } from '../entities/PolluantDtoVisualisation';
 import { DonneesLocalesDto } from '../entities/DonneesLocalesDto';
 import { CommuneAlerte } from '../entities/commune-alerte';
 import { NotificationService } from '../services/notification.service';
+import { IndicateursService } from '../services/indicateursService';
+import { CommuneIndicateur } from '../entities/commune-indicateur';
 
 @Component({
   selector: 'app-details-indicateurs',
@@ -35,10 +37,13 @@ export class DetailsIndicateursPage implements OnInit {
   iconColor: string;
 
   alerte = false;
+  suppressionIndicateurs = false;
+  communeSuppression: CommuneIndicateur;
 
   listePolluantsAlerte: string[] = [];
 
-  constructor(private route: ActivatedRoute, private communeService: CommuneService, private notificationService: NotificationService) {
+  constructor(private route: ActivatedRoute, private communeService: CommuneService, private notificationService: NotificationService,
+    private indicateursService: IndicateursService, private router: Router) {
     this.codeInsee = route.snapshot.paramMap.get('codeInsee');
   }
 
@@ -91,9 +96,38 @@ export class DetailsIndicateursPage implements OnInit {
         this.affichageErreur = true;
       }
     });
+  }
 
+  /**
+   * méthode qui sert à passer à l'affichage de la confirmation de suppression d'un indicateur.
+   * Elle prend en paramètre l'indicateur à supprimer
+   * @param communeIndicateur
+   */
+  supprimerIndicateur(communeIndicateur: CommuneIndicateur) {
+    this.communeSuppression = communeIndicateur;
+  }
 
+  /**
+   * méthode qui permet d'annuler la suppression d'un indicateur et de revenir à l'affichage des indicateurs
+   */
+  annulationSuppressionIndicateur() {
+    this.suppressionIndicateurs = false;
+  }
 
+  /**
+   * Methode qui permet la suppression définitive d'un indicateur.
+   */
+  confirmationSuppressionIndicateur() {
+    this.indicateursService
+      .supprimerIndicateur(this.donneesLocales.commune.nom)
+      .subscribe(
+        result => {
+          this.router.navigate(["/indicateurs"]);
+        },
+        err => {
+          this.router.navigate(["/indicateurs"]);
+        }
+      );
   }
 
 }
